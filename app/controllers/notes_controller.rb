88a569963b5264930fdc6ca8b7ee8b3a4f4fcfd5
github username <github email address>
2020-12-04 12:42:35 -0600

    some more back end stuff

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,25 @@
+class NotesController < ApplicationController 
+
+    def index 
+        @notes = Note.all 
+        render json: @notes
+    end 
+
+    def show 
+        @note = Note.find_by(id: params[:id])
+        render json: @note 
+    end 
+
+    def create
+        @note = Note.create(title: params[:title], content: params[:content])
+        render json: @note 
+    end 
+
+    def destroy
+        @note = Note.find_by(id: params[:id])
+        @note.destroy
+        render json: {status: 200, message: "delted bye bye"}
+    end 
+
+    
+end 
